f69a151bd6e92590d2e8b95f2a090d3c6e2c7ae6
Oscar Garcia <o.garcia@jobin.es>
2019-10-24 18:39:52 +0200

    Add new dependency for manage colors and other stuff, I used this for darken the color in hover state on buttons

--- src/components/basics/button/Button.js
+++ src/components/basics/button/Button.js
@@ -1,63 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 import Lottie from "react-lottie";
-import { green, greenDark } from "../../../constants/colors";
+import { greenDark } from "../../../constants/colors";
+import { darken } from "polished";
 
 import * as cardSpinnerData from "../../../../assets/animations/button-loading.json";
 
+const ButtonStyled = styled.button`
+  border: 2px solid ${props => props.mainColor || greenDark};
+  padding: 12px 24px 12px 24px;
+  height: 56px;
+  background-color: ${props =>
+    props.empty ? "transparent" : props.mainColor || greenDark};
+  border-radius: 4px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${props => (props.empty ? props.mainColor || greenDark : "#fff")};
+  align-items: center;
+  justify-content: center;
+  cursor: ${props => (props.disabled || props.loading ? null : "pointer")};
+  outline: 0px;
+  opacity: ${props => props.disabled || props.loading ? 0.5 : 1.0};
+  ${props => props.style}
+
+  :hover {
+    color: #fff;
+    background-color: ${props => darken(0.05, props.mainColor || greenDark)};
+  }
+`;
+
 const Button = props => {
-    const ButtonStyled = styled.button`
-        border: 2px solid ${props.mainColor || greenDark};
-        padding: 12px 24px 12px 24px;
-        height: 56px;
-        background-color: ${props.empty ? "transparent" : props.mainColor || greenDark};
-        border-radius: 4px;
-        font-family: "Open Sans", sans-serif;
-        font-weight: 600;
-        font-size: 16px;
-        line-height: 24px;
-        color: ${props.empty ? props.mainColor || greenDark : "#fff"};
-        align-items: center;
-        justify-content: center;
-        cursor: ${props =>
-            props.disabled || props.loading ? null : "pointer"};
-        outline: 0px;
-        opacity: ${props.disabled || props.loading ? 0.5 : 1.0};
-        ${props => props.style}
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: cardSpinnerData.default,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
-        :hover {
-            background-color: ${props.mainColor || green};
-            color: #fff;
-            ${
-                props.mainColor && `filter: brightness(150%);`
-            }
-        }
-    `;
-
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: cardSpinnerData.default,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-
-    return (
-        <ButtonStyled
-            {...props}
-            onClick={props.disabled || props.loading ? null : props.onClick}
-        >
-            {props.loading ? (
-                <Lottie
-                    style={{ height: 20, width: 45 }}
-                    options={defaultOptions}
-                />
-            ) : (
-                props.buttonText
-            )}
-        </ButtonStyled>
-    );
+  return (
+    <ButtonStyled
+      {...props}
+      onClick={props.disabled || props.loading ? null : props.onClick}
+    >
+      {props.loading ? (
+        <Lottie style={{ height: 20, width: 45 }} options={defaultOptions} />
+      ) : (
+        props.buttonText
+      )}
+    </ButtonStyled>
+  );
 };
 
 export default Button;
