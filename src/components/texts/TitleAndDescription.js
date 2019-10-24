2d4eee564f7461456d6bae39e69bb3e8d93c3263
Oscar Garcia <o.garcia@jobin.es>
2019-10-24 15:36:32 +0200

    Fix bad logic in icons layout

--- src/components/texts/TitleAndDescription.js
+++ src/components/texts/TitleAndDescription.js
@@ -8,45 +8,44 @@ import styled from "styled-components";
 import device from "../../constants/mediasQueries";
 
 const WrapperStyled = styled.div`
-  display: block;
-  div:first-child {
-    margin-bottom: 10.4px;
-  }
-  @media ${device.mobileL} {
-    ${props =>
-      props.hasIcon
-        ? "display:grid; grid-template-columns: 64px auto; grid-gap:24px;"
-        : "display:block;"}
+    display:block;
 
-    ${props =>
-      props.align &&
-      `
-            ${props.align === "left" &&
-              `
-               
-            `}
-            ${props.align === "right" &&
-              `
-                grid-template-columns: auto 64px ;
-                div:first-child {
-                    order:2;
-                }
-            
-                div:last-child {
-                    order:1;
-                }
-            `}
+    div:first-child {
+        margin-bottom:10.4px;
+    }
 
-            ${props.align === "top" &&
-              `
-                display:block;
+    @media ${device.tablet} {
 
-                div:first-child {
-                    margin-bottom:10.4px;
-                }
-            `}
-        `}
-  }
+        ${props =>
+        props.hasIcon
+            ? "display:grid; grid-template-columns: 64px auto; grid-gap:24px;"
+            : "display:block;"}
+        
+        ${ props => props.align === "left" 
+            ? ''
+            : props.align === "right" 
+                ?   `
+                    grid-template-columns: auto 64px ;
+                    div:first-child {
+                        order:2;
+                    }
+                
+                    div:last-child {
+                        order:1;
+                    }
+                `
+                : props.align === "top" 
+                    ? `
+                    display:block;
+
+                    div:first-child {
+                        margin-bottom:10.4px;
+                    }
+                `
+                    : ''
+        }
+    }
+ 
 `;
 
 const TitleAndDescription = props => {
@@ -54,7 +53,7 @@ const TitleAndDescription = props => {
 
   return (
     <div className={props.className} style={{ ...props.style }}>
-      <WrapperStyled hasIcon={Icon ? true : false}>
+      <WrapperStyled align={props.align} hasIcon={Icon ? true : false}>
         {Icon && (
           <div>
             <Icon mainColor={props.iconMainColor || null} />
