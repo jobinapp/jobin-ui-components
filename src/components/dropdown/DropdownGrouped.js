a4d4d63882cea94b05e9b92954a45686cb2518eb
Oscar Garcia <o.garcia@jobin.es>
2019-10-28 16:24:14 +0100

    Changed font weigth of title in dropdowngrouped component

--- src/components/dropdown/DropdownGrouped.js
+++ src/components/dropdown/DropdownGrouped.js
@@ -12,7 +12,7 @@ const DropdownGrouped = props => {
             onClickDropdown={props.onClickDropdown ? () => props.onClickDropdown(item, i) : null}
             collapsed={props.items[i].collapsed}
           >
-            <h2 style={{fontSize: "18px", margin:0}}>{item.title}</h2>
+            <h2 style={{fontSize: "18px", margin:0, fontWeight: 600}}>{item.title}</h2>
             <P style={{marginTop: 16}}>{item.text}</P>
           </Dropdown>
         );
