77bedc584e6d1d60018184512649f163f7b23cf6
Guille <g.angeles@jobin.es>
2019-11-12 12:12:04 +0100

    v.0.2.63

---
+++ docs/precache-manifest.ae061582a4cc68b6970173220a777727.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec43b4e0069cf2a03fa689cefc9aa8d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cc4a28e371b886fcc539.bundle.js"
+  },
+  {
+    "revision": "5e8505bbf5fc49d95884",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.cc4a28e371b886fcc539.bundle.js"
+  },
+  {
+    "revision": "33d757a9508e97a8db8a35dee35aa095",
+    "url": "static/media/Jober-thumb.33d757a9.png"
+  },
+  {
+    "revision": "e9f89f85a2f1d22eb78af2ebf08a06cd",
+    "url": "static/media/back.e9f89f85.svg"
+  },
+  {
+    "revision": "88d13752f6f2edc8ad30708ff8e06495",
+    "url": "static/media/jober.88d13752.png"
+  },
+  {
+    "revision": "c2a5d89476d8c2feb7ab356c48a72265",
+    "url": "static/media/piscina.c2a5d894.png"
+  },
+  {
+    "revision": "554da283e0b0a50915dbcfa37fb1ef83",
+    "url": "static/media/post-image.554da283.png"
+  },
+  {
+    "url": "vendors~main.cc4a28e371b886fcc539.bundle.js"
+  },
+  {
+    "revision": "a162d5c0bf1656dccb1c",
+    "url": "vendors~main.css"
+  }
+]);
